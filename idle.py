fdca13adefb3508ef40b05a96979a42fb8849cc9
Michael Fogleman <fogleman@gmail.com>
2010-02-26 20:51:47 +0000

    idle time bug fix

--- idle.py
+++ idle.py
@@ -1,20 +1,22 @@
 import sys
 
 if sys.platform == 'win32':
-    from ctypes import Structure, windll, c_uint, sizeof, byref
+    from ctypes import *
     
     class LASTINPUTINFO(Structure):
         _fields_ = [
             ('cbSize', c_uint),
-            ('dwTime', c_uint),
+            ('dwTime', c_int),
         ]
         
     def get_idle_duration():
         lastInputInfo = LASTINPUTINFO()
         lastInputInfo.cbSize = sizeof(lastInputInfo)
-        windll.user32.GetLastInputInfo(byref(lastInputInfo))
-        millis = windll.kernel32.GetTickCount() - lastInputInfo.dwTime
-        return millis / 1000.0
+        if windll.user32.GetLastInputInfo(byref(lastInputInfo)):
+            millis = windll.kernel32.GetTickCount() - lastInputInfo.dwTime
+            return millis / 1000.0
+        else:
+            return 0
 else:
     def get_idle_duration():
         return 0
